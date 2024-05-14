ef6b9bbc80acb8920a83c1947cad83f2511c9a19
natcobbinah <fmg3ckali@gmail.com>
2024-05-14 21:37:05 +0200

    app environment secured with TLS/SSL

--- elearning/settings/prod.py
+++ elearning/settings/prod.py
@@ -9,7 +9,7 @@ env.read_env()
 ADMINS = [
     (env.str("ADMIN_NAME"), env.str("ADMIN_EMAIL")),
 ]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['elearningproject.com','www.elearningproject.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -23,3 +23,8 @@ DATABASES = {
 REDIS_URL = 'redis://cache:6379'
 CACHES['default']['LOCATION'] = REDIS_URL
 CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+# Security
+CSRF_COOKIE_SECURE = True 
+SESSION_COOKIE_SECURE = True 
+SECURE_SSL_REDIRECT = True
